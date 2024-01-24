8c40931b2f443b214d924e6a4efa246e382dcbb0
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-01-24 22:36:38 +0200

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,6 @@
+const navigation=document.querySelector('.top-nav');
+const navigationHeight=
+navigation.offsetHeight;
+document.documentElement.style.setProperty
+('--scroll-padding'
+,navigation +'px');
